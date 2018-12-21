79e1d8aa98c47bcc5c23852f34db488b86894f14
azemyyy <43322664+azemyyy@users.noreply.github.com>
2018-12-21 17:22:34 +0700

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -1,9 +1,9 @@
 <?php
-//ikuti format config di bawah "ticket=tiket_kamu&$
+//ikuti format config di bawah "ticket=tiket_kamu&meid=meid_kamu",
 //jangan ada sepasi
 $config = [
-"ticket=sHp2r312otmCpnGns3mCqJhovNiBinuXr5iFa7Tbgp$
-"ticket=sHp2r312otmCpnGns3mCqJhovNiBiojPr619srOlft$
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBinuXr5iFa7Tbgpawio5qfp-2l4K5iWG0m36fkIqzlX5lh5Wu4I6gtaWUla2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=c33e51283fdd482f",
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBiojPr619srOlftu8sKCvfpyrzYKmdWO1dXVnhI2v24qdf96urZKjtbV_y62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=94249fc4d6f9fb25",
   ];
 
 ?>
