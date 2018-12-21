9c416b5b15226828853bbde327fc3e0fd66be424
azemyyy <43322664+azemyyy@users.noreply.github.com>
2018-12-21 17:15:50 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,9 @@
+<?php
+//ikuti format config di bawah "ticket=tiket_kamu&$
+//jangan ada sepasi
+$config = [
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBinuXr5iFa7Tbgp$
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBiojPr619srOlft$
+  ];
+
+?>
